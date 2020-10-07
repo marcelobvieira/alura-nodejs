989acb03a1c5d6b2ed12d817008753803b449e46
Marcelo Vieira <marcelobarbosavieira@gmail.com>
2020-10-07 15:31:06 -0300

    Utilizando body-parser como middleware

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -33,4 +33,16 @@ module.exports = (app) => {
     
     });
 
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'))
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
 }
