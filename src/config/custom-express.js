54a31f045946691c978e5ddc6e6110cbee024ffc
Marcelo Vieira <marcelobarbosavieira@gmail.com>
2020-10-08 16:39:15 -0300

    Implementando o padrão MVC

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -12,6 +12,7 @@ app.use('/estatico', express.static('src/app/public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
       // look in urlencoded POST bodies and delete it
@@ -24,16 +25,18 @@ app.use(methodOverride(function (req, res) {
 const rotas = require('../app/rotas/rotas');
 rotas(app);
 
+const templates = require('../app/views/templates');
+
 app.use(function (req, resp, next) {
   return resp.status(404).marko(
-      require('../app/views/base/erros/404.marko')
+      templates.base.erro404
   );
 
 });
 
 app.use(function (erro, req, resp, next) {
   return resp.status(500).marko(
-      require('../app/views/base/erros/500.marko')
+      templates.base.erro500
   );
 
 });
